e90faffae2c5497009767e1233215562fc24d46d
Matthew Hoffman <mhoffman@lanl.gov>
2018-04-20 10:16:56 -0600

    Add site compiler and package files, setup script
    	new file:   cosim_builds.sh  # this is used to set up env
    	new file:   etc/spack/linux/compilers.yaml  # this is where compilers will go
    	new file:   etc/spack/packages.yaml # this is where to define external depends (packages from HPC modules)
    
    This initial setup works to build something simple like metis.

---
+++ cosim_builds.sh
@@ -0,0 +1,9 @@
+
+# Set clean path
+export PATH=/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/projects/ic/usr/bin
+
+export SPACK_ROOT=/usr/projects/climate/SHARED_CLIMATE/software/source-build/spack
+export PATH=$SPACK_ROOT/bin:$PATH
+module purge
+module load gcc/6.4.0
+module load cmake
