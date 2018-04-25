7cc3458c8c1361c74c47c154402ceda648ba6def
Matthew Hoffman <mhoffman@lanl.gov>
2018-04-25 15:25:28 -0600

    Get PIO package to install
    Switched from autotools to cmake install, changed pio versions, messed
    with stuff.  Now builds!

--- var/spack/repos/builtin/packages/pio/package.py
+++ var/spack/repos/builtin/packages/pio/package.py
@@ -38,31 +38,87 @@
 # please first remove this boilerplate and all FIXME comments.
 #
 from spack import *
+import fnmatch, os
 
 
-class Pio(Package):
+#class Pio(Package):
+class Pio(CMakePackage):
     """FIXME: Put a proper description of your package here."""
 
     # FIXME: Add a proper url for your package's homepage here.
     homepage = "http://www.example.com"
-    url      = "https://github.com/NCAR/ParallelIO/archive/pio1_7_1.tar.gz"
+    #url      = "https://github.com/NCAR/ParallelIO/archive/pio1_7_1.tar.gz"
+    url      = "https://github.com/NCAR/ParallelIO/archive/pio1_10_0.tar.gz"
 
     # FIXME: Add proper versions and checksums here.
-    version('1.7.1', 'd5e5e36e5fe5ac05685c069a7e421f5b')
+    #version('1.7.1', 'd5e5e36e5fe5ac05685c069a7e421f5b')
+    version('1.10.0', '74bdf5e4abc040fc9e1679095926952f')
 
     # FIXME: Add dependencies if required.
     depends_on('mpi')
     depends_on('parallel-netcdf')
     depends_on('netcdf-fortran')
+    depends_on('netcdf')
 
 
+#    def install(self, spec, prefix):
+#      env['MPICC'] = spec['mpi'].mpicc
+#      env['MPICXX'] = spec['mpi'].mpicxx
+#      env['MPIFC'] = spec['mpi'].mpifc
+#      with working_dir("pio"):
+#        configure('NETCDF_PATH=%s' % self.spec['netcdf-fortran'].prefix, 
+#                  'PNETCDF_PATH=%s' % self.spec['parallel-netcdf'].prefix,      
+#                  '--prefix=%s' % self.spec.prefix)
+#        make(parallel=False)
+#        make('install')
+        
+    def cmake_args(self):
+        # FIXME: Add arguments other than
+        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
+        # FIXME: If not needed delete this function
+        
+        options = []
+        # specify MPI compilers
+        options.extend([
+        '-DCMAKE_C_COMPILER=%s'       % self.spec['mpi'].mpicc,
+        '-DCMAKE_CXX_COMPILER=%s'     % self.spec['mpi'].mpicxx,
+        '-DCMAKE_Fortran_COMPILER=%s' % self.spec['mpi'].mpifc,
+        '-DMPI_BASE_DIR:PATH=%s'      % self.spec['mpi'].prefix
+        ])
+
+        # dependencies
+        options.extend([
+        '-DNetCDF_Fortran_PATH=%s' % self.spec['netcdf-fortran'].prefix,
+        '-DNetCDF_C_PATH=%s' % self.spec['netcdf'].prefix,
+        '-DPnetCDF_PATH=%s' % self.spec['parallel-netcdf'].prefix,
+        '-DLIBZ_PATH=%s' % self.spec['zlib'].prefix,
+        '-DHDF5_PATH=%s' % self.spec['hdf5'].prefix
+        ])
+
+        # add extra tools pio requires :(
+        options.extend([
+        '-DUSER_CMAKE_MODULE_PATH:LIST=/turquoise/usr/projects/climate/SHARED_CLIMATE/software/source-build/CMake_Fortran_utils',
+        '-DGENF90_PATH:LIST=/turquoise/usr/projects/climate/SHARED_CLIMATE/software/source-build/CMake_Fortran_utils'
+        ])
+
+        # install prefix
+        options.extend(['-DCMAKE_INSTALL_PREFIX:PATH=%s' % self.prefix])
+
+        return options
+
+
+    # the build process doesn't actually install anything, do it by hand
     def install(self, spec, prefix):
-      env['MPICC'] = spec['mpi'].mpicc
-      env['MPICXX'] = spec['mpi'].mpicxx
-      env['MPIFC'] = spec['mpi'].mpifc
-      with working_dir("pio"):
-        configure('NETCDF_PATH=%s' % self.spec['netcdf-fortran'].prefix, 
-                  'PNETCDF_PATH=%s' % self.spec['parallel-netcdf'].prefix,      
-                  '--prefix=%s' % self.spec.prefix)
-        make()
-        make('install')
+       #mkdir(prefix) # already created by spack
+       src = "{0}/spack-build/pio".format(self.stage.source_path)
+       print src
+       print prefix
+       print self.prefix
+       files = []
+       files = fnmatch.filter(os.listdir(src),'*.mod')
+       print files
+       files.append('libpio.a')
+       print files
+
+       for f in files:
+           install(join_path(src, f), join_path(self.prefix, f))
